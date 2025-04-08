14e06c02562057bdef8246d39e8bf3d4ce422d56
superspeeder <andy@delusion.org>
2025-04-07 20:30:22 -0700

    experiment 1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,50 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+const fillers = {
+  wrong_synonym: ["wrong", "bad", "upside-down", "inside-out"],
+  land: ["Land", "Planet", "World", "Star System", "Depths", "Skies"],
+  whatland: ["the Dead", "the Living", "the Sun", "Humanity", "Dogs", "Pears", "Trilobites", "Numbers", "Reality", "Unreality", "Air", "Not-Bears", "Western Northeastland"],
+  adjective: ["wonderous", "terrible", "incompetent", "tall", "short", "long", "looooong", "trollish", "frank", "great", "grand", "horrible", "questionable", "curious", "tangerine-like", "ancient"],
+  target: ["Human", "Dog", "Tree", "Frank", "Computer", "Army", "Pit", "Tardigrade", "King", "Queen", "Monarch Butterfly", "Train", "Brain named Brian"],
+  verbed: ["walked", "ran", "lumbered", "stumbled", "flew", "dropped", "delivered itself", "defenestrated", "sailed"],
+  whatdid: ["fire hydrant", "slab of concrete", "journey", "train", "city bus", "boat", "space ship", "hypothetical plane made of numbers"],
+  whatmet: ["talking cat", "dinosaur", "human", "creature made of space dust", "reanimated quilt", "moose"],
+  verbed2: ["jumped", "climbed", "flung themselves", "flew", "phased into reality", "ran", "stepped"],
+  hidingplace: ["rock", "cloud of green mist", "wall", "great wall (of China)", "thin sheet of safety glass"],
+  weapon: ["knife", "machete", "shotgun", "pineapple", "klein bottle", "charcuterie board"],
+  attack: ["stabbed", "shot", "slashed at", "pelted", "hugged", "unraveled the subatomic particles of", "fed"]
+};
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const template = `Before we get to where it all went $wrong_synonym, let me tell you how we got here.
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+Once upon a time, in the $land of $whatland, there was once a $adjective $target. They $verbed away on a great $whatdid, and met many a $whatmet. However, this is where it all went $wrong_synonym.
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+One of the $whatmet wasn't what they appeared to be, and when the our hero least expected it, the $whatmet $verbed2 from behind a $hidingplace with a $weapon, and $attack our hero with it.
+`;
+
+
+// STUDENTS: You don't need to edit code below this line.
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  $("#box").text(story);
+  console.log($("#box"))
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+$("#clicker").click(generate);
+
+generate();
